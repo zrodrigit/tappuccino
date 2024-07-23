f14291c498f9812a54eba32a62f30e619bb417b0
zrodrigit <172134825+zrodrigit@users.noreply.github.com>
2024-07-23 01:03:01 -0500

    set up basic express server

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple web game about forest animals making coffee.
---
+++ backend/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
